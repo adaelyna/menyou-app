baaca2c32a4a511bda7d605a4deabd2f9a9222fa
Dmitry Shevtsov <mail@flyweb.kz>
2023-06-12 21:03:45 +0600

    changed button and moved modal to home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,29 +1,80 @@
 <template>
     <main>
         <h1>Home page</h1>
-        <RouterLink to="/users" class="link">
-            Пользователи
-        </RouterLink>
-        <MButton :label="'Пользователи'" @click="router.push('/users')" />
+        <RouterLink to="/users" class="link"> Пользователи </RouterLink>
+        <div class="buttons">
+            <MButton :label="'Авторизация'" @click="router.push('/login')" />
+            <MButton :label="'Открыть модальное окно'" @click="openModal" />
+        </div>
+        <div v-if="isOpen" class="box">
+            <div class="close" @click="closeModal">x</div>
+            Some content here Some content here Some content here Some content here
+        </div>
     </main>
 </template>
 
 <script setup>
-import { useRouter } from 'vue-router';
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
 import MButton from '../components/ui/MButton.vue'
 
-const router = useRouter()
+const isOpen = ref(false)
 
+const openModal = () => {
+    isOpen.value = true
+}
+
+const closeModal = () => {
+    isOpen.value = false
+}
+
+const router = useRouter()
 </script>
 
 <style scoped lang="scss">
 .link {
     text-decoration: none;
     color: #bebebe;
-    transition: all .5s;
+    transition: all 0.5s;
 
     &:hover {
         color: #7c7c7c;
     }
+}
+
+.box {
+    position: relative;
+    background-color: rgb(235, 235, 235);
+    padding: 24px;
+    border-radius: 16px;
+    max-width: 400px;
+    margin: 0 auto;
+    margin-top: 24px;
+}
+
+.close {
+    font-size: 18px;
+    position: absolute;
+    top: 6px;
+    right: 6px;
+    width: 24px;
+    height: 24px;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: all 0.3s;
+
+    &:hover {
+        transform: rotate(90deg);
+    }
+}
+
+.buttons {
+    display: flex;
+    align-items: center;
+    gap: 20px;
+    padding: 0 10px;
 }
 </style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,66 +3,17 @@
         <div class="container">
             <h1>Авторизация</h1>
             <LoginForm />
-            <MButton :label="'Открыть модальное окно'" @click="openModal" />
-            <div v-if="isOpen" class="box">
-                <div class="close" @click="closeModal">x</div>
-                Some content here
-                Some content here
-                Some content here
-                Some content here
-            </div>
         </div>
     </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-
 import LoginForm from '../components/LoginForm.vue'
-import MButton from '../components/ui/MButton.vue';
-
-const isOpen = ref(false)
-
-const openModal = () => {
-     isOpen.value = true
-}
-
-const closeModal = () => {
-    isOpen.value = false
-}
 </script>
 
 <style scoped lang="scss">
 .login-page {
     background-color: #e7e7e7;
     min-height: 100vh;
-}
-
-.box {
-    position: relative;
-    background-color: white;
-    padding: 24px;
-    border-radius: 16px;
-    max-width: 400px;
-    margin: 0 auto;
-    margin-top: 24px;
-}
-
-.close {
-    font-size: 18px;
-    position: absolute;
-    top: 6px;
-    right: 6px;
-    width: 24px;
-    height: 24px;
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    transition: all .3s;
-
-    &:hover {
-        transform: rotate(90deg);
-    }
 }
 </style>
