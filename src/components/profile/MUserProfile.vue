884f960e615e08227b0c05eb94170639984f268e
Dmitry Shevtsov <mail@flyweb.kz>
2023-07-12 14:35:23 +0600

    added avatar

--- src/components/profile/MUserProfile.vue
+++ src/components/profile/MUserProfile.vue
@@ -1,20 +1,25 @@
 <template>
     <div class="user-profile">
         <MButton color="transparent" @click="handleDropdown">
-            <span class="user-profile__button"> {{ user.firstname }} {{ user.lastname }} </span>
+            <div class="user-profile__button">
+                <div class="user-profile__image">
+                    <img :src="user.image" :alt="user.username" />
+                </div>
+                <span class="user-profile__text"> {{ user.firstname }} {{ user.lastname }} </span>
+            </div>
         </MButton>
 
         <div v-if="isOpen" class="dropdown">
             <ul>
                 <li>
                     <RouterLink to="/">
-                        <img src="@/assets/images/user-icon.svg" alt="Профиль">
+                        <img src="@/assets/images/user-icon.svg" alt="Профиль" />
                         <span>Профиль</span>
                     </RouterLink>
                 </li>
                 <li>
                     <MButton color="transparent" @click="logoutUser">
-                        <img src="@/assets/images/logout-icon.svg" alt="Выйти">
+                        <img src="@/assets/images/logout-icon.svg" alt="Выйти" />
                         <span>Выйти</span>
                     </MButton>
                 </li>
@@ -25,7 +30,7 @@
 
 <script setup>
 import { storeToRefs } from 'pinia'
-import { ref } from 'vue';
+import { ref } from 'vue'
 
 import { useAuthStore } from '../../stores/auth'
 import MButton from '../ui/MButton.vue'
@@ -46,15 +51,36 @@ const logoutUser = () => {
 <style scoped lang="scss">
 .user-profile {
     position: relative;
-    
+
     &__button {
+        display: flex;
+        align-items: center;
+        gap: 12px;
+    }
+
+    &__image {
+        width: 32px;
+        height: 32px;
+        border-radius: 50%;
+        overflow: hidden;
+
+        img {
+            display: block;
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+    }
+
+    &__text {
         font-size: 18px;
     }
 }
 
 .dropdown {
     position: absolute;
-    top: 24px;
+    right: 0;
+    top: 36px;
     background-color: #fff;
     padding: 12px;
     border-radius: 6px;
@@ -65,7 +91,7 @@ const logoutUser = () => {
             margin-bottom: 8px;
 
             a {
-                color: #242424 ;
+                color: #242424;
                 font-size: 16px;
                 display: flex;
                 align-items: center;
